6a58e57f53f71f17d457c00bb23e63ae6aa792b4
Valentina Santa Muñoz <valentinas792@gmail.com>
2023-10-30 20:12:02 -0500

    feat:enum UserType
    Se crea tipo enumerado UserType

---
+++ src/main/java/com/valentina/examportalspringbootapp/model/UserType.java
@@ -0,0 +1,17 @@
+package com.valentina.examportalspringbootapp.model;
+
+public enum UserType {
+
+    ADMIN("ADMIN"), TEACHER("TEACHER"), STUDENT("STUDENT");
+
+    private final String type;
+
+    UserType(String string){
+        type=string;
+    }
+
+    @Override
+    public String toString() {
+        return type;
+    }
+}
