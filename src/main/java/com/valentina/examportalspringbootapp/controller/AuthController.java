377eddebd21ed807f3d07e15f507cdcfdc9ec1b8
Valentina Santa Muñoz <valentinas792@gmail.com>
2023-10-31 21:41:18 -0500

    fix:cambio nombre de la clase
    Se renombra clase para corregir error y se añaden variables necesarias

--- src/main/java/com/valentina/examportalspringbootapp/controller/AuthController.java
+++ src/main/java/com/valentina/examportalspringbootapp/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.valentina.examportalspringbootapp.security.JwtGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/admin")
-public class AdminController {
-
-    @Autowired
-    private JwtGenerator jwtGenerator;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private TeacherRepo teacherRepo;
+public class AuthController {
 
     @Autowired
     private AdminRepo adminRepo;
+    @Autowired
+    private TeacherRepo teacherRepo;
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtGenerator jwtGenerator;
 
     @PostMapping("api/v1/adminRegister")
     public ResponseEntity<String> adminRegister(@RequestBody AdminAuthDto adminAuthDto){
